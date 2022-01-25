f45e939190dd51101aa7bd9c666a6aab6944bb99
cap-codes <vovuchkvn@gmail.com>
2022-01-25 14:18:26 +0200

    Enabling git pages_8

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import react from 'react';
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [paragraph, setParagraph] = useState("Push the button");
+
   return (
     <div className="App">
       <h1>Hello react!</h1>
+      <button onClick={() => setParagraph("Button wroks good!")}>Push me</button>
+      <p>{paragraph}</p>
     </div>
   );
 }
